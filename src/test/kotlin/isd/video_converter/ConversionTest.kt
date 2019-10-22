28d293fa8a82bf527b36d64f3ac8ec7713caf0de
pavelperc <pavelperc@mail.ru>
2019-10-22 22:23:51 +0300

    Added video conversion library and made a converting test.

--- src/test/kotlin/isd/video_converter/ConversionTest.kt
+++ src/test/kotlin/isd/video_converter/ConversionTest.kt
@@ -1,15 +1,41 @@
 package isd.video_converter
 
-import org.amshove.kluent.shouldEqualTo
+import org.amshove.kluent.shouldBeFalse
+import org.amshove.kluent.shouldBePositive
+import org.amshove.kluent.shouldBeTrue
 import org.junit.Test
+import ws.schild.jave.*
+import java.io.File
 
 
 class ConversionTest {
     
-    
+    // https://github.com/a-schild/jave2/wiki/Examples
     @Test
     fun testConversion() {
+        println("working dir: ${System.getProperty("user.dir")}")
+        val source = File("test_video/bunny.mp4")
+        val target = File("test_video/bunny-converted.avi")
+        target.delete()
         
-        (2 + 2) shouldEqualTo 4
+        source.exists().shouldBeTrue()
+        source.length().shouldBePositive()
+        target.exists().shouldBeFalse()
+        
+        val audio = AudioAttributes()
+        val video = VideoAttributes()
+        val attrs = EncodingAttributes()
+        attrs.audioAttributes = audio
+        attrs.videoAttributes = video
+        
+        // for speed-up
+        video.setCodec(VideoAttributes.DIRECT_STREAM_COPY)
+        attrs.format = "avi"
+        val instance = Encoder()
+        instance.encode(MultimediaObject(source), target, attrs, null)
+        
+        source.exists().shouldBeTrue()
+        target.exists().shouldBeTrue()
+        target.length().shouldBePositive()
     }
 }
---
+++ test_video/readme.txt
@@ -0,0 +1,2 @@
+To run the tests, you should copy test files from here:
+https://yadi.sk/d/8OhAPEuwmZxbCA
